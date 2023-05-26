423f00de11d04031b8686dfd25a746b8c1b10733
Michael Humphries <michaelhumphries1997@gmail.com>
2023-05-26 14:41:51 +0100

    adding package

--- configure.js
+++ configure.js
@@ -1,15 +1,30 @@
-const os = require("os");
-const paths = require("./paths.json");
+#!/usr/bin/env node
 
-/**
- * 1. get home dir
- * 2. Take location of configs and work out relative dir of vscode configs from home dir,
- * assuming vscode has been setup in users home.
- * 3. put files in the right place using config.
- */
-const args = process.argv.length > 0 && process.argv.slice(2);
-const configsDir = args[0] || "./";
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
+const configPaths = require("./paths.json");
 
 const root = os.homedir();
 
-console.log(__dirname, configsDir);
+const SETTINGS_FILE = "settings.json";
+const KEYBINDINGS_FILE = "keybindings.json";
+
+/**
+ * 1. get home dir
+ * 2. read in paths
+ * 3. move file to homedir + path
+ */
+const args = process.argv.length > 0 && process.argv.slice(2);
+// user input doesnt work atm, not sure I'll even keep it.
+const userInputVsCodeConfigsDir = args[0] || __dirname;
+
+const keybindings = path.join(__dirname, KEYBINDINGS_FILE);
+const settings = path.join(__dirname, SETTINGS_FILE);
+
+const vscodeKeybindingsLocation = path.join(root, configPaths.keybindings);
+const vscodeSettingsLocation = path.join(root, configPaths.settings);
+
+// perform move
+fs.copyFileSync(keybindings, vscodeKeybindingsLocation);
+fs.copyFileSync(settings, vscodeSettingsLocation);
